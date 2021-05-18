853ef358fec28bf122e4e5bcd99edc991979b248
DivineBee <vizanttriss@gmail.com>
2021-05-18 11:09:26 +0300

    add owners list and update controller. closes #15

--- pet-clinic-web/src/main/java/triss/springframework/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/triss/springframework/petclinic/controllers/OwnerController.java
@@ -1,7 +1,9 @@
 package triss.springframework.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import triss.springframework.petclinic.services.OwnerService;
 
 /**
  * @author Beatrice V.
@@ -12,8 +14,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
+
         return "/owners/index";
     }
 }
