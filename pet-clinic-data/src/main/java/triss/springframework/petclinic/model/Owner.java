57d300b900a115c407ce757c506d2c640986225f
DivineBee <vizanttriss@gmail.com>
2021-05-23 17:23:30 +0300

    add visits, update owner with pet relationship. closes #23

--- pet-clinic-data/src/main/java/triss/springframework/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/triss/springframework/petclinic/model/Owner.java
@@ -1,9 +1,21 @@
 package triss.springframework.petclinic.model;
 
+import java.util.Set;
+
 /**
  * @author Beatrice V.
  * @created 11.05.2021 - 16:30
  * @project pet-clinic
  */
 public class Owner extends Person{
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
