6d822d755b044328cdb4189688ca2ee0c06cde08
DivineBee <vizanttriss@gmail.com>
2021-05-23 17:36:35 +0300

    add PetType service . closes #26

---
+++ pet-clinic-data/src/main/java/triss/springframework/petclinic/services/PetTypeService.java
@@ -0,0 +1,11 @@
+package triss.springframework.petclinic.services;
+
+import triss.springframework.petclinic.model.PetType;
+
+/**
+ * @author Beatrice V.
+ * @created 23.05.2021 - 17:31
+ * @project pet-clinic
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/triss/springframework/petclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,40 @@
+package triss.springframework.petclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import triss.springframework.petclinic.model.PetType;
+import triss.springframework.petclinic.services.PetTypeService;
+
+import java.util.Set;
+
+/**
+ * @author Beatrice V.
+ * @created 23.05.2021 - 17:32
+ * @project pet-clinic
+ */
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
