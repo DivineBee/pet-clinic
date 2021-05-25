a6f1aebef47807311f94cfb1f763f86f388076a8
DivineBee <vizanttriss@gmail.com>
2021-05-25 20:37:05 +0300

    add specialties map impl. closes #28

---
+++ pet-clinic-data/src/main/java/triss/springframework/petclinic/services/SpecialtiesService.java
@@ -0,0 +1,11 @@
+package triss.springframework.petclinic.services;
+
+import triss.springframework.petclinic.model.Specialty;
+
+/**
+ * @author Beatrice V.
+ * @created 25.05.2021 - 20:29
+ * @project pet-clinic
+ */
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/triss/springframework/petclinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,41 @@
+package triss.springframework.petclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import triss.springframework.petclinic.model.Specialty;
+import triss.springframework.petclinic.services.SpecialtiesService;
+
+import java.util.Set;
+
+/**
+ * @author Beatrice V.
+ * @created 25.05.2021 - 20:31
+ * @project pet-clinic
+ */
+@Service
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+}
