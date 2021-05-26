106a62cd0c0cebc3572169bbfdef8eea753728ae
DivineBee <vizanttriss@gmail.com>
2021-05-26 12:11:29 +0300

    convert vets to JPA. closes #34

--- pet-clinic-data/src/main/java/triss/springframework/petclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/triss/springframework/petclinic/model/Specialty.java
@@ -1,12 +1,19 @@
 package triss.springframework.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * @author Beatrice V.
  * @created 23.05.2021 - 17:24
  * @project pet-clinic
  */
+@Entity
+@Table(name = "specialty")
 public class Specialty extends BaseEntity{
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/triss/springframework/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/triss/springframework/petclinic/model/Vet.java
@@ -1,5 +1,6 @@
 package triss.springframework.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -8,7 +9,13 @@ import java.util.Set;
  * @created 11.05.2021 - 16:30
  * @project pet-clinic
  */
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn("specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {
