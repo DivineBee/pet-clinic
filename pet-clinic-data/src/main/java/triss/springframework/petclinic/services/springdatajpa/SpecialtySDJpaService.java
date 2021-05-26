c4a897a21488897227f4259e970004d861b1800a
DivineBee <vizanttriss@gmail.com>
2021-05-26 16:23:25 +0300

    create specialty JPA service. closes #41

---
+++ pet-clinic-data/src/main/java/triss/springframework/petclinic/services/springdatajpa/SpecialtySDJpaService.java
@@ -0,0 +1,53 @@
+package triss.springframework.petclinic.services.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import triss.springframework.petclinic.model.Specialty;
+import triss.springframework.petclinic.repositories.SpecialtyRepository;
+import triss.springframework.petclinic.services.SpecialtyService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Beatrice V.
+ * @created 26.05.2021 - 16:20
+ * @project pet-clinic
+ */
+@Service
+@Profile("springdatajpa")
+public class SpecialtySDJpaService implements SpecialtyService {
+
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtySDJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> specialties = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialties::add);
+        return specialties;
+    }
+
+    @Override
+    public Specialty findById(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
