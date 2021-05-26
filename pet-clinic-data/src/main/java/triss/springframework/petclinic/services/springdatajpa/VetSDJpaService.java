2cf5f1e068970136b4faf70b14c3954d3e48a012
DivineBee <vizanttriss@gmail.com>
2021-05-26 16:11:26 +0300

    create vet JPA service. closes #38

---
+++ pet-clinic-data/src/main/java/triss/springframework/petclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,53 @@
+package triss.springframework.petclinic.services.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import triss.springframework.petclinic.model.Vet;
+import triss.springframework.petclinic.repositories.VetRepository;
+import triss.springframework.petclinic.services.VetService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Beatrice V.
+ * @created 26.05.2021 - 16:07
+ * @project pet-clinic
+ */
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}
