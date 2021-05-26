aafed46e663fc4ca26daab44e5ea5f5268cbc7f5
DivineBee <vizanttriss@gmail.com>
2021-05-26 16:39:10 +0300

    create visit map service. closes #43

---
+++ pet-clinic-data/src/main/java/triss/springframework/petclinic/services/VisitService.java
@@ -0,0 +1,11 @@
+package triss.springframework.petclinic.services;
+
+import triss.springframework.petclinic.model.Visit;
+
+/**
+ * @author Beatrice V.
+ * @created 26.05.2021 - 16:31
+ * @project pet-clinic
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+}
